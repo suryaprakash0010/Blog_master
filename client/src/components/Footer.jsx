c9c4502d07c0fbe4bad609e6d0586fb29cd54c42
Surya Prakash Kahar <sp2002rk@gmail.com.com>
2025-07-23 10:54:19 +0530

    added CopyRight Footer section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3'>
+        <div className='flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500'>
+        <div>
+            <img src="" alt="" />
+        </div>
+
+        </div>
+        <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 © Blog_masters - All Rights Reserved. </p>
     </div>
   )
 }
