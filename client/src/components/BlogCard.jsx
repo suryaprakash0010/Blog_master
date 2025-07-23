a2f808527d081ffe452e7f1fedabf13f4641ff07
Surya Prakash Kahar <sp2002rk@gmail.com.com>
2025-07-23 10:37:51 +0530

    created News_letter email box and Subscribe button

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -12,7 +12,8 @@ const BlogCard = ({blog}) => {
 <span className='ml-5 mt-4 px-3 py-1 inline-block bg-primary/20 rounded-full text-primary text-xs'>{category}</span>
     <div className='p-5'>
 <h5 className='mb-2 font-medium text-gray-900'>{title}</h5>
-<p className='mb-3 text-xs text-gray-600'>{description.slice(0,80)}</p>
+<p className='mb-3 text-xs text-gray-600'
+dangerouslySetInnerHTML={{"__html":description.slice(0,80)}}></p>
     </div>
     
     </div>
