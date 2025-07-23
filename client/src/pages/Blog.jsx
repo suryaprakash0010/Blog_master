2ae952b179e6ee5321f9742b19fd83611deaae31
Surya Prakash Kahar <sp2002rk@gmail.com.com>
2025-07-23 18:43:03 +0530

    Added blog date,image and Description

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import { assets } from '../assets/assets'
+import Moment from 'moment'
+import {blog_data} from '../assets/assets' 
 
 const Blog = () => {
-  return (
-    <div>
-        <h1>Blog</h1>
+
+  const {id} = useParams()
+
+  const [data, setData]=useState(null)
+
+  const fetchBlogData = async()=> {
+    const data = blog_data.find(item=> item._id === id)
+    setData(data) 
+  }
+
+  useEffect(()=>{
+    fetchBlogData()
+  },[])
+
+  return data ? (
+    <div className='relative'>
+      <img src={assets.gradientBackground} alt="" className='absolute top-0 -z-10 w-full opacity-50' />
+      <Navbar/>
+      <div className='text-center mt-20 text-gray-600'>
+          <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+          <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
+          <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
+         <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/35 font-medium text-primary'>Michael Brown </p>
+      </div>
+
+      <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
+        <img src={data.image} alt="" className='rounded-3xl mb-5'/>
+         <div dangerouslySetInnerHTML={{__html: data.description}}></div>
+      </div>
     </div>
-  )
+  ) : <div>loading...</div>
 }
 
 export default Blog
