f5c8152df035e04635ab5b1f395c79bf9f76794e
Surya Prakash Kahar <sp2002rk@gmail.com.com>
2025-07-26 12:22:05 +0530

    Created Addblog section input title and Description options

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { assets, footer_data } from '../assets/assets'
 
 const Footer = () => {
-  return (
+return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3'>
         <div className='flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500'>
         <div>
-            <img src={assets.logo} alt="logo" className='w-32 sm:w-44' />
-            <p className='max-w-[410px] mt-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, enim a? Sed harum mollitia consectetur adipisci quidem accusantium a veniam vitae, tenetur suscipit aperiam alias modi voluptatum optio ad eius.</p>
+            <img src={assets.logo} alt="logo" className='w-52 sm:w-72 ' />
+            <p className='max-w-[410px] mt-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, enim a? Sed harum mollitia consectetur adipisci Lorem, ipsum dolor..</p>
         </div>
         <div className='flex flex-wrap justify-between w-full md:w-[45%] gap-5'>
             {footer_data.map((section, index)=> (
@@ -16,7 +16,7 @@ const Footer = () => {
             <ul className='text-sm space-y-1'>
                 {section.links.map((link,i)=> (
                     <li key={i}>
-                     <a href="#" className='hover:underline transition'>{link}</a>
+                    <a href="#" className='hover:underline transition'>{link}</a>
                     </li>
                 ))}
             </ul>
--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -13,7 +13,7 @@ const Layout = () => {
   return (
   <>
   <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
-  <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer ' onClick={()=> navigate('/')} /> 
+  <img src={assets.logo} alt="" className='className="w-52 sm:w-72  cursor-pointer ' onClick={()=> navigate('/')} /> 
   <button onClick={logout} className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer'>Logout</button> 
   </div>
   <div className='flex h-[calc(100vh-70px)]'>
