67f3d57cd5f1863fbac651e397f8ca76988e8564
Surya Prakash Kahar <sp2002rk@gmail.com.com>
2025-07-26 23:51:21 +0530

    build admin_Controller for backend with basic admin operations

---
+++ server/controllers/adminContoller.js
@@ -0,0 +1,14 @@
+export const adminLogin =async (req , res)=>{
+    try{
+        const {email,password} = req.body;
+
+        if(email !==process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD){
+            return res.json({success: false, message: "Invalid Credentials"})
+        }
+        const token = JsonWebTokenError.sign({email}, process.env.JWT_SECRET)
+        res.json({success:true , token})
+
+    } catch (error){
+res.json({success: false , message:error.message})
+    }
+}
