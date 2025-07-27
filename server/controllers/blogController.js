95ea6228a009546c414c04ecaacbc4985b326238
Surya Prakash Kahar <sp2002rk@gmail.com.com>
2025-07-27 14:38:29 +0530

    Created back_end Add blog and Delete blog options

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -44,3 +44,41 @@ export const addBlog = async(req,res)=>{
        res.json({success: false , message: error.message})
     }
 }
+
+export const getAllBlogs = async (req,res)=>{
+    
+    try{
+        const blogs = await Blog.find({isPublished:true})
+        res.json({success: true , blogs})
+    } catch(error) {
+        res.json({success: false , message: error.message})
+    }
+}
+
+//for individual blog data
+export const getBlogById = async(req,res) =>{
+    try{
+        const {blogId} = req.parse;
+        const blog = await Blog.findById(blogId)
+        if(!blog){
+            return res.json({success: false, message : "Blog not found "});
+
+        }
+        res.json({success: true , blog})
+    }  catch (error){
+       res.json({success: false, message:error.message})
+    }
+}
+
+
+
+
+export const deleteBlogById = async(req,res) =>{
+    try{
+        const {id} = req.body;
+        await Blog.findByIdAndDelete(id);
+        res.json({success: true  , message: 'Blog deleted successfully'})
+    }  catch (error){
+       res.json({success: false, message:error.message})
+    }
+}
