2fb71c8f556373895b473d3775f5e13d580c0daa
Surya Prakash Kahar <sp2002rk@gmail.com.com>
2025-07-27 21:50:55 +0530

    Connected Back-end with Front-end

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,35 @@
+import { useContext, useState } from "react"
+import { createContext } from "react"
+import axios from "axios";
+import {useNavigate} from 'react-router-dom'
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+
+const AppContext = createContext();
+
+
+export const AppProvider = ({ children }) =>{
+
+    const navigate = useNavigate()
+
+    const [token, setToken] =useState(null)
+    const [blogs, setBlogs] =useState(null)
+    const [input, setInput] =useState("")
+
+     const value = {
+        axios ,navigate, token , setToken, blogs ,setBlogs, input, setInput  
+     }
+    return (
+
+        
+
+        <AppContext.Provider value={value}>
+            { children }
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = ()=>{
+return useContext (AppContext)
+};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,12 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+import { AppProvider } from './context/AppContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <AppProvider>
     <App />
+  </AppProvider>
   </BrowserRouter>,
 )
