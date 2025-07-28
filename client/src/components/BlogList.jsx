54c0087c0730e51766405b3286111d4852b3ab41
Surya Prakash Kahar <sp2002rk@gmail.com.com>
2025-07-28 17:43:44 +0530

    Added back-end search functionality for blogs

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -2,9 +2,18 @@ import React, { useState } from 'react'
 import { blog_data, blogCategories } from '../assets/assets'
 import { motion } from "framer-motion"
 import BlogCard from './BlogCard'
+import { useAppContext } from '../context/AppContext'
 
 const BlogList = () => {
 const [menu,setMenu] = useState("All")
+const {blogs , input}= useAppContext()
+
+  const filteredBlogs = ()=>{
+    if (!blogs) return []; 
+  if (input === '') return blogs;
+    
+    return blogs.filter((blog)=> blog.title.toLowerCase().includes(input.toLowerCase()) || blog.category.toLowerCase().includes(input.toLowerCase()))
+  } 
 
   return (
     <div>
@@ -24,7 +33,7 @@ const [menu,setMenu] = useState("All")
 ))}
 </div>
 <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40' >
-{blog_data.filter((blog)=> menu ==="All" ? true : blog.category===menu).map
+{filteredBlogs().filter((blog)=> menu ==="All" ? true : blog.category===menu).map
 ((blog)=> <BlogCard key={blog._id} blog={blog}/>
 )}
         </div>
--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -7,10 +7,15 @@ import {blog_data} from '../assets/assets'
 import { comments_data } from '../assets/assets'
 import Footer from '../components/Footer'
 import Loader from '../components/Loader'
+import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast'
 
 const Blog = () => {
 
   const {id} = useParams()
+
+
+  const {axios} = useAppContext()
 
   const [data, setData]=useState(null)
   
@@ -20,16 +25,46 @@ const Blog = () => {
   const [content, setContent]=useState('')
 
   const fetchBlogData = async()=> {
-    const data = blog_data.find(item=> item._id === id)
-    setData(data) 
+    try {
+      const {data} = await axios.get(`/api/blog/${id}`)
+      data.success ? setData(data.blog) : toast.error(data.message)
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   const fetchComments =async ()=>{
-    setComments(comments_data)
+   
+    try {
+       const {data} = await axios.post('/api/blog/comments' , {blogId : id})
+       if(data.success){
+        setComments(data.comments)
+       }
+       else{
+        toast.error(data.message);
+       }
+    } catch (error) {
+              toast.error(error.message);
+
+    }
   }
 
   const addComment =async(e)=>{
     e.preventDefault();
+
+    try {
+             const {data} = await axios.post('/api/blog/add-comments' , {blog : id,name ,content});
+              if(data.success){
+toast.success(data.message)      
+setName('')
+setContent('')      
+ }
+ else{
+  toast.error(data.message);
+ }
+    } catch (error) {
+        toast.error(error.message);
+    }
   }
 
   useEffect(()=>{
