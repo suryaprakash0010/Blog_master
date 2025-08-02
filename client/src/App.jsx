c5cba7710c28bf32689d92c33486bdc1cfe85e88
Surya Prakash Kahar <sp2002rk@gmail.com.com>
2025-08-02 08:50:23 +0530

    Fix casing issue ListBlog.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Layout from './pages/admin/Layout';
 import Dashboard from './pages/admin/Dashboard';
-import ListBlog from './pages/admin/ListBlog';
+import ListBlog from './pages/admin/ListingBlog';
 import Comments from './pages/admin/Comments';
 import Login from './components/admin/Login';
 import 'quill/dist/quill.snow.css';
